7896fb07956f70a7c99fea31d50b6a52d6fb769b
aaron huynh <huynh.aaron@gmail.com>
2017-05-15 19:18:59 -0700

    Fix index.ios.js error

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,4 @@
-
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry} from 'react-native';
 import App from './src/app';
 
 AppRegistry.registerComponent('liftables', () => App);
-
